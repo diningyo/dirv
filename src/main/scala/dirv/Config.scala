894f9b2f4da273ffcf76d153c7c642af765fb382
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-03-03 23:36:29 +0900

    add RISC-V arch parameters

--- src/main/scala/dirv/Config.scala
+++ src/main/scala/dirv/Config.scala
@@ -4,16 +4,38 @@ package dirv
 
 import dirv.io.{MemRIO, MemRWIO}
 
+case object Consts {
+  val rv32XLen = 32
+  val rviRegNum = 32
+  val rveRegNum = 16
+}
+
+// arch
+sealed abstract trait RVArch {
+  def xLen(): Int
+  def regNum(): Int
+}
+case object RV32I extends RVArch {
+  override def xLen(): Int = Consts.rv32XLen
+  override def regNum(): Int = Consts.rviRegNum
+}
+case object RV32E extends RVArch {
+  override def xLen(): Int = Consts.rv32XLen
+  override def regNum(): Int = Consts.rveRegNum
+}
+
 case object defaultConfig {
+  val initAddr = BigInt("80000000", 16)
+  val rv32Bits = 32
   val addrBits = 32
   val dataBits = 32
-  val xLen = 32
 }
 
 case class Config(
+    arch: RVArch = RV32I,
+    initAddr: BigInt = defaultConfig.initAddr,
     addrBits: Int = defaultConfig.addrBits,
     dataBits: Int = defaultConfig.dataBits,
-    xLen: Int = defaultConfig.xLen,
     dbg: Boolean = true
 ) {
   val imemIOType = MemRIO
