78deb70c3e9b86e131bfed4e536cde9095e2c417
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-03-03 23:34:31 +0900

    modify I/O port definition

--- src/main/scala/dirv/io/Bundles.scala
+++ src/main/scala/dirv/io/Bundles.scala
@@ -13,14 +13,14 @@ case object MemRWIO extends MemIOType
 
 object MemCmd {
   val len = 1
-  val wr = 0x0
-  val rd = 0x1
+  val wr = 0x0.U
+  val rd = 0x1.U
 }
 
 object MemResp {
   val len = 1
-  val ok = 0x0
-  val err = 0x1
+  val ok = 0x0.U
+  val err = 0x1.U
 }
 
 class MemBaseIO(addrBits: Int) extends Bundle {
@@ -34,7 +34,7 @@ class MemBaseIO(addrBits: Int) extends Bundle {
 }
 
 class MemR(dataBits: Int) extends Bundle {
-  val data = Output(UInt(dataBits.W))
+  val data = Input(UInt(dataBits.W))
   val rddv = Input(Bool())
 
   override def cloneType: MemR.this.type =
@@ -42,6 +42,7 @@ class MemR(dataBits: Int) extends Bundle {
 }
 
 class MemW(dataBits: Int) extends Bundle {
+  val strb = Output(UInt((dataBits / 8).W))
   val data = Output(UInt(dataBits.W))
   val ack = Input(Bool())
 
@@ -51,14 +52,14 @@ class MemW(dataBits: Int) extends Bundle {
 
 class MemIO(ioType: MemIOType, addrBits: Int, dataBits: Int) extends MemBaseIO(addrBits) {
   val w = ioType match {
-    case _: MemRIO.type => None
-    case _: MemWIO.type => Some(new MemW(dataBits))
-    case _: MemRWIO.type => Some(new MemW(dataBits))
+    case MemRIO => None
+    case MemWIO => Some(new MemW(dataBits))
+    case MemRWIO => Some(new MemW(dataBits))
   }
   val r = ioType match {
-    case _: MemRIO.type => Some(new MemR(dataBits))
-    case _: MemWIO.type => None
-    case _: MemRWIO.type => Some(new MemR(dataBits))
+    case MemRIO => Some(new MemR(dataBits))
+    case MemWIO => None
+    case MemRWIO => Some(new MemR(dataBits))
   }
 
   override def cloneType: MemIO.this.type =
@@ -72,7 +73,8 @@ object MemIO {
 
 
 class DbgIO(implicit  cfg: Config) extends Bundle {
-  val pc = UInt(cfg.addrBits.W)
+  val pc = Output(UInt(cfg.addrBits.W))
+  val fin = Output(Bool())
 }
 
 object DbgIO {
@@ -82,7 +84,7 @@ object DbgIO {
 class DirvIO(implicit cfg: dirv.Config) extends Bundle {
   val imem = MemIO(cfg.imemIOType, cfg.addrBits, cfg.dataBits)
   val dmem = MemIO(cfg.dmemIOType, cfg.addrBits, cfg.dataBits)
-  val dbg = DbgIO
+  val dbg = if (cfg.dbg) Some(new DbgIO) else None
 
   override def cloneType: DirvIO.this.type =
     new DirvIO().asInstanceOf[this.type]
