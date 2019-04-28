a6422d2bbbebf08e1dedba46dbdc349ce20e0387
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-04-29 00:24:53 +0900

    implement Lsu size select logic.

--- src/main/scala/dirv/pipeline/Lsu.scala
+++ src/main/scala/dirv/pipeline/Lsu.scala
@@ -25,17 +25,26 @@ class Lsu(implicit cfg: Config) extends Module {
   val io = IO(new LsuIO())
 
   val inst = io.exu2lsu.inst
-
-  io.lsu2ext.valid := false.B
-  io.lsu2ext.cmd := MemCmd.rd.U
-  io.lsu2ext.addr := io.exu2lsu.memAddr
-  io.lsu2ext.w.get.data := 0xdeafbeafL.U
-  io.lsu2ext.w.get.strb := 0xf.U
-  io.lsu2ext.r.get.ready := true.B
-  io.lsu2ext.size := Mux1H(Seq(
+  val size = Mux1H(Seq(
     (inst.sb || inst.lb || inst.lbu) -> MemSize.byte.U,
     (inst.sh || inst.lh || inst.lhu) -> MemSize.half.U,
     (inst.sw || inst.lw) -> MemSize.byte.U
   ))
+
+  val uaMsb = log2Ceil(cfg.arch.xlen / 8)
+  val unaligedAddr = io.exu2lsu.memAddr(uaMsb - 1, 0)
+  val strb = Mux1H(Seq(
+    inst.sb -> Fill(0x1 << MemSize.byte, 1.U),
+    inst.sh -> Fill(0x1 << MemSize.half, 1.U),
+    inst.sw -> Fill(0x1 << MemSize.word, 1.U)
+  )) << unaligedAddr
+
+  io.lsu2ext.valid := inst.loadValid || inst.storeValid
+  io.lsu2ext.cmd := Mux(inst.loadValid, MemCmd.rd.U, MemCmd.wr.U)
+  io.lsu2ext.addr := io.exu2lsu.memAddr
+  io.lsu2ext.w.get.data := 0xdeafbeafL.U
+  io.lsu2ext.w.get.strb := strb
+  io.lsu2ext.r.get.ready := true.B
+  io.lsu2ext.size := size
   io.lsu2ext.w.get.valid := io.exu2lsu.inst.storeValid
 }
