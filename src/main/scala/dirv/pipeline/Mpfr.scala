c77c5a845d702a418880def11dd9ad26200dbdf7
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-04-28 21:33:44 +0900

    change mpfr implementations (xregs number is changed 31 to 32 including "zero" register)

--- src/main/scala/dirv/pipeline/Mpfr.scala
+++ src/main/scala/dirv/pipeline/Mpfr.scala
@@ -5,33 +5,53 @@ package dirv.pipeline
 import chisel3._
 import dirv.Config
 
-class MpfrIO(addrBits: Int, dataBits:Int) extends Bundle {
+
+/**
+  * Register Source I/O
+  * @param addrBits address width
+  * @param dataBits data width
+  */
+class RsIO(addrBits: Int, dataBits:Int) extends Bundle {
   val addr = Input(UInt(addrBits.W))
   val data = Output(UInt(dataBits.W))
+
+  override def cloneType: RsIO.this.type = new RsIO(addrBits, dataBits).asInstanceOf[this.type]
 }
 
-class Mpfr(implicit cfg: Config) extends Module with InstInfo {
-  val io = IO(new Bundle {
-    val rs1 = new MpfrIO(rs1Bits, cfg.dataBits)
-    val rs2 = new MpfrIO(rs2Bits, cfg.dataBits)
-    val wb = new Bundle {
-      val en = Input(Bool())
-      val addr = Input(UInt(rs1Bits.W))
-      val data = Input(UInt(cfg.dataBits.W))
-    }
-    val fin = if (cfg.dbg) Some(Output(Bool())) else None
-  })
+/**
+  * File Register I/O
+  * @param cfg
+  */
+class MpfrIO(implicit val cfg: Config) extends Bundle {
+  val rs1 = new RsIO(cfg.arch.mpfrBits, cfg.arch.xlen)
+  val rs2 = new RsIO(cfg.arch.mpfrBits, cfg.arch.xlen)
+  val rd = new Bundle {
+    val en = Input(Bool())
+    val addr = Input(UInt(cfg.arch.mpfrBits.W))
+    val data = Input(UInt(cfg.dataBits.W))
+  }
+  val xregs = if (cfg.dbg) Some(Output(Vec(cfg.arch.regNum, UInt(cfg.arch.xlen.W)))) else None
 
-  val xRegs = RegInit(VecInit(Seq.fill(cfg.arch.regNum-1)(0.U(cfg.addrBits.W))))
+  override def cloneType: MpfrIO.this.type = new MpfrIO().asInstanceOf[this.type]
+}
 
-  io.rs1.data := Mux(io.rs1.addr === 0.U, 0.U, xRegs(io.rs1.addr-1.U))
-  io.rs2.data := Mux(io.rs2.addr === 0.U, 0.U, xRegs(io.rs2.addr-1.U))
+/**
+  * File Register
+  * @param cfg
+  */
+class Mpfr(implicit cfg: Config) extends Module with InstInfoRV32 {
+  val io = IO(new MpfrIO())
 
-  when (io.wb.en) {
-    xRegs(io.wb.addr-1.U) := io.wb.data
+  val xRegs = RegInit(VecInit(Seq.fill(cfg.arch.regNum)(0.U(cfg.arch.xlen.W))))
+
+  io.rs1.data := xRegs(io.rs1.addr)
+  io.rs2.data := xRegs(io.rs2.addr)
+
+  when (io.rd.en && (io.rd.addr =/= 0.U)) {
+    xRegs(io.rd.addr) := io.rd.data
   }
 
   if (cfg.dbg) {
-    io.fin.get := (xRegs(3) === 0x1.U)
+    io.xregs.get := xRegs
   }
 }
