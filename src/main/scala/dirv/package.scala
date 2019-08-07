6de8d585240571f6a430da7fdda471932ed76040
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-08-07 23:10:17 +0900

    add package object file for dirv.

---
+++ src/main/scala/dirv/package.scala
@@ -0,0 +1,12 @@
+// See LICENSE for license details.
+
+package object dirv {
+  type MemCmd = io.MemCmd.type
+  val MemCmd = io.MemCmd
+  type MemSize = io.MemSize.type
+  val memSize = io.MemSize
+  type MemIO = io.MemIO
+  val MemRIO = io.MemRIO
+  val MemWIO = io.MemWIO
+  val MemRWIO = io.MemRWIO
+}
