af3f490eeb197745105da620f60a158b3e4183dd
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-09-15 23:56:42 +0900

    change each Queue module for IO to MbusSliece.

--- src/main/scala/mbus/MbusDecoder.scala
+++ src/main/scala/mbus/MbusDecoder.scala
@@ -65,20 +65,22 @@ class MbusDecoder(p: MbusDecoderParams) extends Module {
 
   val io = IO(new MbusDecoderIO(p))
 
-  val m_cmd_q = Queue(io.in.c, 1, true, true)
-  val m_rd_q = Queue(io.out(0).r.get, 1, true, true)
-  val m_wr_q = Queue(io.in.w.get, 1, true, true)
+  val m_in_slice = Module(new MbusSlice(p.ioAttr, p.addrBits, p.dataBits,
+    cSlice = false, rSlice = false, wSlice = false))
+
+  m_in_slice.io.in <> io.in
 
   for ((out_port, info) <- io.out zip p.addrMap) {
-    out_port.c <> m_cmd_q
-    out_port.w.get <> m_wr_q
+    out_port.c <> m_in_slice.io.out.c
 
-    val w_port_sel = checkAddress(m_cmd_q.bits.addr, info)
-    out_port.c.valid := m_cmd_q.valid && w_port_sel
+    val w_port_sel = checkAddress(m_in_slice.io.out.c.bits.addr, info)
+    out_port.c.valid := m_in_slice.io.out.c.valid && w_port_sel
+
+    out_port.w.get <> m_in_slice.io.out.w.get
 
     val r_wr_sel = RegNext(w_port_sel, false.B)
 
-    when (!w_port_sel && m_wr_q.fire()) {
+    when (!w_port_sel && m_in_slice.io.out.w.get.fire()) {
       r_wr_sel := false.B
     }
 
@@ -87,16 +89,14 @@ class MbusDecoder(p: MbusDecoderParams) extends Module {
     out_port.w.get.valid := w_wr_valid
 
     // out(N).r.get.ready reflects io.in.r.get.ready
-    out_port.r.get.ready := io.in.r.get.ready
+    out_port.r.get.ready := m_in_slice.io.out.r.get.ready
   }
-
-  m_rd_q.ready := io.in.r.get.ready
 
   // read
   val w_rd_valid = io.out.map(_.r.get.valid)
   val w_rd_data = io.out.map(_.r.get.bits)
-  io.in.r.get.valid := w_rd_valid.reduce(_ || _)
+  m_in_slice.io.out.r.get.valid := w_rd_valid.reduce(_ || _)
 
   val w_dummy = 0.U.asTypeOf(chiselTypeOf(io.in.r.get.bits))
-  io.in.r.get.bits := MuxCase(w_dummy, w_rd_valid zip w_rd_data)
+  m_in_slice.io.out.r.get.bits := MuxCase(w_dummy, w_rd_valid zip w_rd_data)
 }
