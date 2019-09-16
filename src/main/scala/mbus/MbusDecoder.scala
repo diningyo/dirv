0dcae680f851e3f26cadfc02c1da2269abb1b4ef
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-09-16 18:42:44 +0900

    bug fixed. out(N).w.get.valid becomes 1'b1, when Host issues read command.

--- src/main/scala/mbus/MbusDecoder.scala
+++ src/main/scala/mbus/MbusDecoder.scala
@@ -77,23 +77,26 @@ class MbusDecoder(p: MbusDecoderParams) extends Module {
     val w_port_sel = checkAddress(m_in_slice.io.out.c.bits.addr, info)
     w_port_sel.suggestName(s"w_port_sel_${idx}")
 
+    val w_wr_req = w_port_sel && (m_in_slice.io.out.c.bits.cmd === MbusCmd.wr.U)
+    w_wr_req.suggestName(s"w_wr_req_${idx}")
+
     out_port.c.valid := m_in_slice.io.out.c.valid && w_port_sel
 
     out_port.w.get <> m_in_slice.io.out.w.get
 
-    val r_wr_sel = RegNext(w_port_sel, false.B)
+    val r_wr_sel = RegNext(w_wr_req, false.B)
     r_wr_sel.suggestName(s"r_wr_sel_${idx}")
 
-    when (!w_port_sel && m_in_slice.io.out.w.get.fire()) {
+    when (!w_wr_req && m_in_slice.io.out.w.get.fire()) {
       r_wr_sel := false.B
     }
 
-    val w_wr_valid = dontTouch(w_port_sel || r_wr_sel)
+    val w_wr_valid = dontTouch(w_wr_req || r_wr_sel)
     w_wr_valid.suggestName(s"w_wr_valid_${idx}")
 
     out_port.w.get.valid := w_wr_valid
 
-    // out(N).r.get.ready reflects io.in.r.get.ready
+    // out(N).r.get.ready reflects m_in_slice.io.out.r.get.ready
     out_port.r.get.ready := m_in_slice.io.out.r.get.ready
   }
 
