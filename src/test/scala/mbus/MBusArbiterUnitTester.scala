2ed411582f58990f02b284fc3caff09a0b6726ee
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-09-17 23:28:27 +0900

    add single read test.

--- src/test/scala/mbus/MBusArbiterUnitTester.scala
+++ src/test/scala/mbus/MBusArbiterUnitTester.scala
@@ -24,6 +24,7 @@ class MbusArbiterUnitTester(c: SimDTMMbusArbiter) extends PeekPokeTester(c) {
       poke(in(idx).r.get.ready, true)
     }
 
+    poke(c.io.dut.out.r.get.valid, false)
     poke(c.io.dut.out.w.get.ready, true)
     step(cycle)
   }
@@ -77,14 +78,21 @@ class MbusArbiterUnitTester(c: SimDTMMbusArbiter) extends PeekPokeTester(c) {
     * MemIO read request
     * @param addr Address to read
     */
-  def read_req(addr: Int): Unit = {
+  def read_req(port: Int, addr: BigInt): Unit = {
+    poke(in(port).c.valid, true)
+    poke(in(port).c.bits.addr, addr)
+    poke(in(port).c.bits.cmd, MbusCmd.rd)
+    poke(in(port).c.bits.size, MbusSize.word)
   }
 
   /**
     * Sram return read data
     * @param data Data to read
     */
-  def return_read_data(data: Int): Unit = {
+  def return_read_data(data: BigInt): Unit = {
+    poke(out.r.get.valid, true)
+    poke(out.r.get.bits.data, data)
+    poke(out.r.get.bits.resp, MbusResp.ok)
   }
 
   /**
@@ -92,7 +100,24 @@ class MbusArbiterUnitTester(c: SimDTMMbusArbiter) extends PeekPokeTester(c) {
     * @param addr Address to write
     * @param exp expect value for read register
     */
-  def single_read(addr: Int, exp: Int, rdDataLatency: Int = 0): Unit = {
+  def single_read(port: Int, addr: BigInt, exp: BigInt, rdDataLatency: Int = 0): Unit = {
+    read_req(port, addr)
+    if (rdDataLatency == 0) {
+      return_read_data(exp)
+    }
+    step(1)
+
+    poke(in(port).c.valid, false)
+
+    if (rdDataLatency != 0) {
+      step(rdDataLatency - 1)
+      return_read_data(exp)
+      expect(in(port).r.get.valid, true)
+      expect(in(port).r.get.bits.data, exp)
+      step(1)
+    }
+    poke(out.r.get.valid, false)
+    step(1)
   }
 
   /**
@@ -139,6 +164,31 @@ class MbusArbiterTester extends BaseTester {
             val addr = intToUnsignedBigInt(r.nextInt())
             val data = intToUnsignedBigInt(r.nextInt())
             single_write(idx, addr, 0xf, data, delay)
+          }
+        }
+        idle(10)
+
+      }
+    } should be (true)
+  }
+
+  it should "single read. [rd:100]" in {
+
+    val outDir = dutName + "-100"
+    val args = getArgs(Map(
+      "--top-name" -> dutName,
+      "--target-dir" -> s"test_run_dir/$outDir"
+    ))
+
+    Driver.execute(args, () => new SimDTMMbusArbiter(base_p)(timeoutCycle)) {
+      c => new MbusArbiterUnitTester(c) {
+        idle(10)
+
+        for (delay <- 0 until 10) {
+          for (idx <- 0 until base_p.numOfMasters) {
+            val addr = intToUnsignedBigInt(r.nextInt())
+            val data = intToUnsignedBigInt(r.nextInt())
+            single_read(idx, addr, data, delay)
           }
         }
         idle(10)
