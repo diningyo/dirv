d3158681e57f70f99c3eae3e27a7c07ca963105d
diningyo <diningyo.kpuku.jougeki@gmail.com>
2020-03-09 23:54:31 +0900

    Xtorに乱数モジュールを追加

--- src/test/scala/mbus/SimDTMMbusIC.scala
+++ src/test/scala/mbus/SimDTMMbusIC.scala
@@ -3,10 +3,27 @@
 package mbus
 
 import chisel3._
+import chisel3.util._
+import chisel3.util.random.MaxPeriodGaloisLFSR
 import test.util._
 
-class Xtor(ioType: MbusIOAttr, addrBits: Int, dataBits: Int) extends Module {
+class Xtor(ioType: MbusIOAttr, addrBits: Int, dataBits: Int)(randBits: Int, seed: Int) extends Module {
   val io = MbusIO(ioType, addrBits, dataBits)
+
+  val m_glfsr = Module(new MaxPeriodGaloisLFSR(randBits, Some(1)))
+
+  val r_rand_valid = RegInit(false.B)
+
+  r_rand_valid := true.B
+
+  m_glfsr.io.seed.valid := r_rand_valid
+  m_glfsr.io.seed.bits := VecInit(seed.U.asBools())
+
+  val w_rand_data = m_glfsr.io.out
+
+  io.c.valid := true.B
+  io.c.bits := true.B
+
 }
 
 /**
